5b21e018c1b28c670a893c8d306daca50f655f19
Tomasz Heflik <heflik@gmail.com>
2017-09-29 15:54:49 +0200

    Export IP before building

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "In test bash script"
 MONGODB_HOST="$(/sbin/ip route|awk '/default/ { print $3 }')"
-echo $MONGODB_HOST
+echo "Using IP:" ${MONGODB_HOST}
 export MONGODB_HOST=$MONGODB_HOST
-env
